615c78c108f908716f17aa99ec4ca859f7d21759
Janez Urevc <janez@janezurevc.name>
2014-04-20 15:03:12 +0200

    by slashrsm: Add plupload_test module.

---
+++ tests/plupload_test/lib/Drupal/plupload/PluploadTestForm.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\plupload_test\PluploadTestForm
+ */
+
+namespace Drupal\plupload;
+
+use Drupal\Core\Form\FormInterface;
+
+/**
+ * Plupload test form class.
+ */
+class PluploadTestForm implements FormInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return '_plupload_test_form';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, array &$form_state) {
+    $form['plupload'] = array(
+      '#type' => 'plupload'
+    );
+
+    $form['submit'] = array(
+      '#type' => 'submit',
+      '#value' => t('Submit'),
+    );
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, array &$form_state) {}
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, array &$form_state) {
+    dpm($form_state);
+  }
+}
